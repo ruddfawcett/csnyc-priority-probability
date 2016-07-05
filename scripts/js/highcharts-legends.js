046d07bf0c1d797d256cba83a1035624e2ba0602
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-07-05 17:54:09 -0400

    Toggle between categories

--- scripts/js/highcharts-legends.js
+++ scripts/js/highcharts-legends.js
@@ -1,26 +1,55 @@
 var setup = function(chart) {
-  // console.log(chart);
-  $legend = $('.legend');
+  setupGroups(chart);
+  setupCategories(chart);
+};
 
+var setupGroups = function(chart) {
+  $legend = $('.legend.group');
 
   $.each(chart.series, function (j, series) {
-    // console.log(series);
-
-    var li = $('<li/>').text('Group '+(j+1)).appendTo($legend);
-    var checkbox = $('<input data-index="'+j+'" type="checkbox" checked />').prependTo(li);
-
-    // $legend.append()
-      // $legend.append('<div class="item"><div class="symbol" style="background-color:'+data.color+'"></div><div class="serieName" id="">' + data.statement + '</div></div>');
+    var li = $('<li/>');
+    var checkbox = $('<input id="group-'+j+'" data-index="'+j+'" type="checkbox" checked />').appendTo(li);
+    var label = $('<label class="color-'+j+'" for="group-'+j+'"/>').text('Group '+(j+1)).appendTo(li);
+    li.appendTo($legend);
   });
 
-  $('.legend li input[type="checkbox"]').change(function () {
+  $('.legend.group li input[type="checkbox"]').change(function () {
     $index = $(this).data('index');
     series = chart.series[$index];
 
     series.setVisible(!series.visible);
   });
-
-  $('.legend li').click(function() {
-
-  })
 };
+
+var setupCategories = function(chart) {
+  var categories = ['Community', 'Curriculum', 'Equity', 'Implementation', 'Research & Evaluation', 'Sustainability & Scale', 'Teacher Development'];
+  $legend = $('.legend.category');
+
+  $.each(categories, function(i, category) {
+    var li = $('<li/>');
+    var checkbox = $('<input id="category-'+i+'" data-index="'+i+'" type="checkbox" checked />').appendTo(li);
+    var label = $('<label class="color-'+i+'" for="category-'+i+'"/>').text(categories[i]).appendTo(li);
+    li.appendTo($legend);
+  });
+
+  $('.legend.category li input[type="checkbox"]').change(function () {
+    $index = $(this).data('index');
+    var checked = this.checked;
+
+    console.log(chart.series);
+
+    $.each(chart.series, function(j, series) {
+      $.each(series.points, function(k, point) {
+        console.log(point);
+        if (point.csnyc_category == categories[$index]) {
+          if (checked) {
+            point.update({marker: {radius: 28}});
+          }
+          else {
+            point.update({marker: {radius: 0}});
+          }
+        }
+      });
+    });
+  });
+}
